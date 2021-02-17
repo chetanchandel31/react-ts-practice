47c767fce1b71ea6c4d4368a5bb658e630e7bc0a
chetanchandel31 <chetanchandel3@gmail.com>
2021-02-17 20:37:13 +0530

    added a loader

--- src/components/Anime-List/index.tsx
+++ src/components/Anime-List/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import Anime from './Anime';
 import './style.css';
 import SadPikachu from '../images/tenor.gif';
+import WaitingShinchan from '../images/shinchan.gif';
 
 type AnimeObj = {
     Type: string;
@@ -20,54 +21,80 @@ type AnimeListProps = {
 
 const AnimeList: React.FC<AnimeListProps> = ({anime, setAnime, list, filterValue}) => {
     const [error, setError] = React.useState<boolean>(false);
+    const [loading, setLoading] = React.useState<boolean>(false);
 
     useEffect(
         () => {
-            list.forEach((animeName: string) => {
-            let ourURL: string = `https://www.omdbapi.com/?apikey=3c9e3d41&s=${encodeURIComponent(animeName)}&type=${filterValue}`;
+            setLoading(true);
+            setError(false);
+
+            setTimeout(() => {
+                list.forEach((animeName: string) => {
             
-            fetch(ourURL)
-            .then(res => res.json())
-            .then(res => {
-                let animeArr: AnimeObj[] = res.Search;
-                if (!animeArr) return setError(true);
-                setError(false);
-                setAnime((prevAnime: AnimeObj[]) => {
-                    let combinedArr = [...prevAnime, ...animeArr];
-                    let filteredArr: AnimeObj[] = [];
-                    combinedArr.forEach(c => {
-                        let foundOrNot: number = filteredArr.findIndex(f => f.imdbID === c.imdbID);
-                        if (foundOrNot === -1) filteredArr.push(c);
-                    })
-                    return filteredArr;
-                });
-            });
-            })// eslint-disable-next-line
-        }, [setAnime, list]//why setAnime
+                    let ourURL: string = `https://www.omdbapi.com/?apikey=3c9e3d41&s=${encodeURIComponent(animeName)}&type=${filterValue}`;
+                    
+                    fetch(ourURL)
+                    .then(res => res.json())
+                    .then(res => {
+                        let animeArr: AnimeObj[] = res.Search;
+                        if (!animeArr) return setError(true);
+                        
+                        setAnime((prevAnime: AnimeObj[]) => {
+                            let combinedArr = [...prevAnime, ...animeArr];
+                            let filteredArr: AnimeObj[] = [];
+                            combinedArr.forEach(c => {
+                                let foundOrNot: number = filteredArr.findIndex(f => f.imdbID === c.imdbID);
+                                if (foundOrNot === -1) filteredArr.push(c);
+                            })
+                            return filteredArr;
+                        });
+                    });
+                })
+                
+                setLoading(false);
+            }, 1000)
+
+             // eslint-disable-next-line
+        }, [list]//why setAnime
     )
 
-    if(error === false) {
-        return(
-            <div className ='animeList'>
-                {anime.map((animeObj: AnimeObj) => {
-                    if (animeObj) {
-                        let animeComp = <Anime key={animeObj.imdbID}
-                        Title={animeObj.Title}
-                        Poster={animeObj.Poster}
-                        Year={animeObj.Year}/>
-                        
-                        return animeComp
-                    } else return null
-                })}
-            </div>
+    let loader: JSX.Element = (
+        <div className='loader'>
+            <img src={WaitingShinchan} alt='' className='waitingShinchan' />
+            <h1>loadingg...</h1>
+        </div>
     )
-    } else return (
+
+    let animeList: JSX.Element = (
+        <div className ='animeList'>
+        {anime.map((animeObj: AnimeObj) => {
+            if (animeObj) {
+                let animeComp = <Anime key={animeObj.imdbID}
+                Title={animeObj.Title}
+                Poster={animeObj.Poster}
+                Year={animeObj.Year}/>
+                
+                return animeComp
+            } else return null
+        })}
+    </div>
+    )
+
+    let errorMessage: JSX.Element = (
         <div className='errorMessage'>
             <img src={SadPikachu} alt='' className='sadPikachu' />
             <h4>couldn't find any results that match your search</h4>
-        </div>
+        </div> 
     )
-        
+
+    return(
+        <>
+            {!loading && !error? animeList:
+            loading? loader:
+            errorMessage}
+        </>
+    )
+
 }
 
 export default AnimeList;
